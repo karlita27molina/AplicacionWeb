6b3f78e55d32697aefb55877eae9da9c3ebd9c94
karlita27molina <klmolina5@espe.edu.ec>
2021-09-15 13:47:27 -0500

    Primer Commit

--- direccion.php
+++ direccion.php
@@ -41,7 +41,7 @@
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-lg-8 col-xl-6 text-center">
-                        <h2 class="text-white mt-0">Registrate para hacer tu pedido</h2>
+                        <h2 class="text-white mt-0">Registre los Datos para el pedidor</h2>
                         <hr class="divider" />
                     </div>
                 </div>
